fde5320a6225479be0659cd0dbc385211913daa1
Kelly Copley <copleykj@gmail.com>
2021-04-29 21:55:36 -0400

    setup code style, linting, and husky hooks

--- package.js
+++ package.js
@@ -1,47 +1,47 @@
 /* global Package Npm */
 Package.describe({
-	summary: 'Render your app before the DDP connection even comes alive - magic?',
-	version: '4.0.0',
-	git: 'https://github.com/Meteor-Community-Packages/meteor-fast-render',
-	name: 'communitypackages:fast-render',
-})
+  summary: 'Render your app before the DDP connection even comes alive - magic?',
+  version: '4.0.0',
+  git: 'https://github.com/Meteor-Community-Packages/meteor-fast-render',
+  name: 'communitypackages:fast-render',
+});
 
 Npm.depends({
-	'cookie-parser': '1.4.5',
-})
+  'cookie-parser': '1.4.5',
+});
 
 Package.onUse(function (api) {
-	api.versionsFrom('METEOR@1.6.1')
+  api.versionsFrom('METEOR@1.6.1');
 
-	api.use(['communitypackages:picker@1.1.0', 'lamhieu:meteorx@2.0.1'], 'server')
-	api.use('communitypackages:inject-data@2.3.2', ['client', 'server'])
-	api.use(['livedata', 'webapp', 'routepolicy', 'random'], 'server')
-	api.use(['ecmascript', 'server-render', 'accounts-base', 'ejson', 'minimongo'], ['client', 'server'])
+  api.use(['communitypackages:picker@1.1.0', 'lamhieu:meteorx@2.0.1'], 'server');
+  api.use('communitypackages:inject-data@2.3.2', ['client', 'server']);
+  api.use(['livedata', 'webapp', 'routepolicy', 'random'], 'server');
+  api.use(['ecmascript', 'server-render', 'accounts-base', 'ejson', 'minimongo'], ['client', 'server']);
 
-	api.mainModule('lib/client/fast_render.js', 'client')
-	api.mainModule('lib/server/fast_render.js', 'server')
-})
+  api.mainModule('lib/client/fast_render.js', 'client');
+  api.mainModule('lib/server/fast_render.js', 'server');
+});
 
 Package.onTest(function (api) {
-	api.use(['ecmascript'], ['client', 'server'])
-	api.use('communitypackages:fast-render', ['client', 'server'])
-	api.use('tinytest', ['client', 'server'])
-	api.use('http', 'server')
-	api.use('random', ['server', 'client'])
-	api.use('mongo', ['server', 'client'])
-	api.use('server-render', ['server', 'client'])
+  api.use(['ecmascript'], ['client', 'server']);
+  api.use('communitypackages:fast-render', ['client', 'server']);
+  api.use('tinytest', ['client', 'server']);
+  api.use('http', 'server');
+  api.use('random', ['server', 'client']);
+  api.use('mongo', ['server', 'client']);
+  api.use('server-render', ['server', 'client']);
 
-	api.addFiles(
-		[
-			'tests/utils.js',
-			'tests/client/fast_render.js',
-			'tests/client/ddp_update.js',
-		],
-		'client'
-	)
+  api.addFiles(
+    [
+      'tests/utils.js',
+      'tests/client/fast_render.js',
+      'tests/client/ddp_update.js',
+    ],
+    'client',
+  );
 
-	api.addFiles(
-		['tests/server/context.js', 'tests/server/integration.js'],
-		'server'
-	)
-})
+  api.addFiles(
+    ['tests/server/context.js', 'tests/server/integration.js'],
+    'server',
+  );
+});
--- tests/server/integration.js
+++ tests/server/integration.js
@@ -1,240 +1,240 @@
 /* global Tinytest, Npm */
-import { FastRender } from 'meteor/communitypackages:fast-render'
-import { InjectData } from 'meteor/communitypackages:inject-data'
-import { Random } from 'meteor/random'
-import { Meteor } from 'meteor/meteor'
-import { HTTP } from 'meteor/http'
-import { check } from 'meteor/check'
+import { FastRender } from 'meteor/communitypackages:fast-render';
+import { InjectData } from 'meteor/communitypackages:inject-data';
+import { Random } from 'meteor/random';
+import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http';
+import { check } from 'meteor/check';
 
 Tinytest.add('integration - with a simple route', function (test) {
-	const collName = Random.id()
-	const pubName = Random.id()
-	const path = '/' + Random.id()
-	const obj = { _id: 'one', aa: 10 }
+  const collName = Random.id();
+  const pubName = Random.id();
+  const path = '/' + Random.id();
+  const obj = { _id: 'one', aa: 10 };
 
-	const coll = new Meteor.Collection(collName)
-	coll.insert(obj)
+  const coll = new Meteor.Collection(collName);
+  coll.insert(obj);
 
-	Meteor.publish(pubName, function () {
-		return coll.find()
-	})
+  Meteor.publish(pubName, function () {
+    return coll.find();
+  });
 
-	FastRender.route(path, function () {
-		this.subscribe(pubName)
-	})
+  FastRender.route(path, function () {
+    this.subscribe(pubName);
+  });
 
-	const data = getFRData(path)
-	test.isTrue(data.subscriptions[pubName])
-	test.equal(data.collectionData[collName][0][0], obj)
-})
+  const data = getFRData(path);
+  test.isTrue(data.subscriptions[pubName]);
+  test.equal(data.collectionData[collName][0][0], obj);
+});
 
 Tinytest.add('integration - onAllRoutes', function (test) {
-	const collName = Random.id()
-	const pubName = Random.id()
-	const path = '/' + Random.id()
-	const obj = { _id: 'one', aa: 10 }
+  const collName = Random.id();
+  const pubName = Random.id();
+  const path = '/' + Random.id();
+  const obj = { _id: 'one', aa: 10 };
 
-	const coll = new Meteor.Collection(collName)
-	coll.insert(obj)
+  const coll = new Meteor.Collection(collName);
+  coll.insert(obj);
 
-	const cursorHandler = createCursorHandler(function () {
-		return coll.find()
-	})
+  const cursorHandler = createCursorHandler(function () {
+    return coll.find();
+  });
 
-	Meteor.publish(pubName, function () {
-		return cursorHandler.get()
-	})
+  Meteor.publish(pubName, function () {
+    return cursorHandler.get();
+  });
 
-	FastRender.onAllRoutes(function () {
-		this.subscribe(pubName)
-	})
+  FastRender.onAllRoutes(function () {
+    this.subscribe(pubName);
+  });
 
-	const data = getFRData(path)
-	test.isTrue(data.subscriptions[pubName])
-	test.equal(data.collectionData[collName][0][0], obj)
-	cursorHandler.stop()
-})
+  const data = getFRData(path);
+  test.isTrue(data.subscriptions[pubName]);
+  test.equal(data.collectionData[collName][0][0], obj);
+  cursorHandler.stop();
+});
 
 Tinytest.add('integration - onAllRoutes + route ', function (test) {
-	const collName = Random.id()
-	const pubName = Random.id()
-	const path = '/' + Random.id()
-	const obj1 = { _id: 'one', aa: 10 }
-	const obj2 = { _id: 'two', aa: 10 }
+  const collName = Random.id();
+  const pubName = Random.id();
+  const path = '/' + Random.id();
+  const obj1 = { _id: 'one', aa: 10 };
+  const obj2 = { _id: 'two', aa: 10 };
 
-	const coll = new Meteor.Collection(collName)
-	coll.insert(obj1)
-	coll.insert(obj2)
+  const coll = new Meteor.Collection(collName);
+  coll.insert(obj1);
+  coll.insert(obj2);
 
-	const cursorHandler = createCursorHandler(function (id) {
-		return coll.find({ _id: id })
-	})
+  const cursorHandler = createCursorHandler(function (id) {
+    return coll.find({ _id: id });
+  });
 
-	Meteor.publish(pubName, function (id) {
-		check(id, String)
-		return cursorHandler.get(id)
-	})
+  Meteor.publish(pubName, function (id) {
+    check(id, String);
+    return cursorHandler.get(id);
+  });
 
-	FastRender.onAllRoutes(function () {
-		this.subscribe(pubName, 'one')
-	})
+  FastRender.onAllRoutes(function () {
+    this.subscribe(pubName, 'one');
+  });
 
-	FastRender.route(path, function () {
-		this.subscribe(pubName, 'two')
-	})
+  FastRender.route(path, function () {
+    this.subscribe(pubName, 'two');
+  });
 
-	const data = getFRData(path)
-	test.isTrue(data.subscriptions[pubName])
-	test.equal(data.collectionData[collName][0][0], obj1)
-	test.equal(data.collectionData[collName][1][0], obj2)
-	cursorHandler.stop()
-})
+  const data = getFRData(path);
+  test.isTrue(data.subscriptions[pubName]);
+  test.equal(data.collectionData[collName][0][0], obj1);
+  test.equal(data.collectionData[collName][1][0], obj2);
+  cursorHandler.stop();
+});
 
 Tinytest.add('integration - null publications', function (test) {
-	const collName = Random.id()
-	const path = '/' + Random.id()
-	const obj = { _id: 'one', aa: 10 }
+  const collName = Random.id();
+  const path = '/' + Random.id();
+  const obj = { _id: 'one', aa: 10 };
 
-	const coll = new Meteor.Collection(collName)
-	coll.insert(obj)
+  const coll = new Meteor.Collection(collName);
+  coll.insert(obj);
 
-	const cursorHandler = createCursorHandler(function () {
-		return coll.find()
-	})
-	Meteor.publish(null, function () {
-		return cursorHandler.get()
-	})
+  const cursorHandler = createCursorHandler(function () {
+    return coll.find();
+  });
+  Meteor.publish(null, function () {
+    return cursorHandler.get();
+  });
 
-	const data = getFRData(path)
-	test.equal(data.collectionData[collName][0][0], obj)
-	cursorHandler.stop()
-})
+  const data = getFRData(path);
+  test.equal(data.collectionData[collName][0][0], obj);
+  cursorHandler.stop();
+});
 
 Tinytest.add('integration - send data via this.* apis', function (test) {
-	const collName = Random.id()
-	const pubName = Random.id()
-	const path = '/' + Random.id()
-	const obj = { _id: 'one', aa: 10 }
+  const collName = Random.id();
+  const pubName = Random.id();
+  const path = '/' + Random.id();
+  const obj = { _id: 'one', aa: 10 };
 
-	Meteor.publish(pubName, function () {
-		const sub = this
-		const { _id, ...newObj } = obj
-		sub.added(collName, _id, newObj)
-		Meteor.setTimeout(function () {
-			sub.ready()
-		}, 100)
-	})
+  Meteor.publish(pubName, function () {
+    const sub = this;
+    const { _id, ...newObj } = obj;
+    sub.added(collName, _id, newObj);
+    Meteor.setTimeout(function () {
+      sub.ready();
+    }, 100);
+  });
 
-	FastRender.route(path, function () {
-		this.subscribe(pubName)
-	})
+  FastRender.route(path, function () {
+    this.subscribe(pubName);
+  });
 
-	const data = getFRData(path)
-	test.isTrue(data.subscriptions[pubName])
-	test.equal(data.collectionData[collName][0][0], obj)
-})
+  const data = getFRData(path);
+  test.isTrue(data.subscriptions[pubName]);
+  test.equal(data.collectionData[collName][0][0], obj);
+});
 
 Tinytest.add('integration - send data via this.* apis, but delayed', function (
-	test
+  test,
 ) {
-	const collName = Random.id()
-	const pubName = Random.id()
-	const path = '/' + Random.id()
-	const obj = { _id: 'one', aa: 10 }
+  const collName = Random.id();
+  const pubName = Random.id();
+  const path = '/' + Random.id();
+  const obj = { _id: 'one', aa: 10 };
 
-	Meteor.publish(pubName, function () {
-		const sub = this
-		Meteor.setTimeout(function () {
-			const { _id, ...newObj } = obj
-			sub.added(collName, _id, newObj)
-			sub.ready()
-		}, 1000)
-	})
+  Meteor.publish(pubName, function () {
+    const sub = this;
+    Meteor.setTimeout(function () {
+      const { _id, ...newObj } = obj;
+      sub.added(collName, _id, newObj);
+      sub.ready();
+    }, 1000);
+  });
 
-	FastRender.route(path, function () {
-		this.subscribe(pubName)
-	})
+  FastRender.route(path, function () {
+    this.subscribe(pubName);
+  });
 
-	const data = getFRData(path)
-	test.isFalse(data.subscriptions[pubName])
-	test.equal(data.collectionData, {})
-})
+  const data = getFRData(path);
+  test.isFalse(data.subscriptions[pubName]);
+  test.equal(data.collectionData, {});
+});
 
 Tinytest.add('integration - error inside a publication', function (test) {
-	const collName = Random.id()
-	const pubName = Random.id()
-	const path = '/' + Random.id()
-	const obj = { _id: 'one', aa: 10 }
+  const collName = Random.id();
+  const pubName = Random.id();
+  const path = '/' + Random.id();
+  const obj = { _id: 'one', aa: 10 };
 
-	const coll = new Meteor.Collection(collName)
-	coll.insert(obj)
+  const coll = new Meteor.Collection(collName);
+  coll.insert(obj);
 
-	Meteor.publish(pubName, function () {
-		throw new Error('some bad thing happens')
-	})
+  Meteor.publish(pubName, function () {
+    throw new Error('some bad thing happens');
+  });
 
-	FastRender.route(path, function () {
-		this.subscribe(pubName)
-	})
+  FastRender.route(path, function () {
+    this.subscribe(pubName);
+  });
 
-	const data = getFRData(path)
-	test.equal(data.collectionData, {})
-})
+  const data = getFRData(path);
+  test.equal(data.collectionData, {});
+});
 
 Tinytest.add('integration - error inside a null publication', function (test) {
-	const collName = Random.id()
-	const path = '/' + Random.id()
-	const obj = { _id: 'one', aa: 10 }
+  const collName = Random.id();
+  const path = '/' + Random.id();
+  const obj = { _id: 'one', aa: 10 };
 
-	const coll = new Meteor.Collection(collName)
-	coll.insert(obj)
+  const coll = new Meteor.Collection(collName);
+  coll.insert(obj);
 
-	Meteor.publish(null, function () {
-		throw new Error('some bad thing happens')
-	})
+  Meteor.publish(null, function () {
+    throw new Error('some bad thing happens');
+  });
 
-	const data = getFRData(path)
-	test.equal(data.collectionData, {})
-})
+  const data = getFRData(path);
+  test.equal(data.collectionData, {});
+});
 
 Tinytest.add('integration - when path has no leading slash', function (test) {
-	const path = Random.id()
+  const path = Random.id();
 
-	test.throws(function () {
-		FastRender.route(path, function () { })
-	}, 'Error: path (' + path + ') must begin with a leading slash "/"')
-})
+  test.throws(function () {
+    FastRender.route(path, function () { });
+  }, 'Error: path (' + path + ') must begin with a leading slash "/"');
+});
 
-const urlResolve = Npm.require('url').resolve
+const urlResolve = Npm.require('url').resolve;
 function getFRData (path) {
-	const url = urlResolve(process.env.ROOT_URL, path)
-	const options = {
-		headers: {
-			Accept: 'text/html',
-		},
-	}
-	const res = HTTP.get(url, options)
+  const url = urlResolve(process.env.ROOT_URL, path);
+  const options = {
+    headers: {
+      Accept: 'text/html',
+    },
+  };
+  const res = HTTP.get(url, options);
 
-	const encodedData = res.content.match(/data">(.*)<\/script/)[1]
-	return InjectData._decode(encodedData)['fast-render-data']
+  const encodedData = res.content.match(/data">(.*)<\/script/)[1];
+  return InjectData._decode(encodedData)['fast-render-data'];
 }
 
 function createCursorHandler (callback) {
-	let stop = false
-	function getFn () {
-		if (stop) {
-			return []
-		} else {
-			return callback.apply(this, arguments)
-		}
-	}
+  let stop = false;
+  function getFn () {
+    if (stop) {
+      return [];
+    } else {
+      return callback.apply(this, arguments);
+    }
+  }
 
-	function stopFn () {
-		stop = true
-	}
+  function stopFn () {
+    stop = true;
+  }
 
-	return {
-		get: getFn,
-		stop: stopFn,
-	}
+  return {
+    get: getFn,
+    stop: stopFn,
+  };
 }

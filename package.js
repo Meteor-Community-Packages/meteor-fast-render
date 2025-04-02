93c9931f38e3a3c0ac1686ac81c28c975425c6fb
Kelly Copley <copleykj@gmail.com>
2025-04-02 12:53:43 -0400

    update version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* global Package Npm */
 Package.describe({
   summary: 'Render your app before the DDP connection even comes alive - magic?',
-  version: '5.0.0-beta.1',
+  version: '5.0.0-beta.2',
   git: 'https://github.com/Meteor-Community-Packages/meteor-fast-render',
   name: 'communitypackages:fast-render',
 });
